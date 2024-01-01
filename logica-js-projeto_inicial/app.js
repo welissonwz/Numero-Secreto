212ff7a9346ece91b49e2f465c15a22a430ac044
Welisson Santos <Welisson_martinss@hotmail.com>
2023-12-31 23:13:15 -0300

    projeto inicial

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,29 @@
+alert("Seja bem vindo ao jogo do Número Secreto!")
+let numeroMaximo=250;
+let numeroSecreto = parseInt(Math.random ()*numeroMaximo+1);
+console.log(numeroSecreto);
+let chute;
+let Tentativas = 1;
+while (chute != numeroSecreto){
+    chute = prompt(`Escolha entre 100 e ${numeroMaximo}`);
+    if(chute == numeroSecreto){
+    break;  
+    }else{
+        if(chute > numeroSecreto){
+        alert(`O número secreto é menor que ${chute}`);
+        }else {
+          alert(`o número secreto é maior que ${chute}`);
+        }      
+        //tentaivas = tentativas+1;
+        Tentativas++  
+    }
+}
+let PalavraTentativa = Tentativas > 1? 'Tentativas' : 'Tentativa';
+    alert(`Parabéns você acertou! O Número Secreto é  ${numeroSecreto} com ${Tentativas} ${PalavraTentativa}`)
+//if(Tentativas>1){
+  //  alert(`Parabéns você acertou! O Número Secreto é  ${numeroSecreto} com ${Tentativas} Tentativas`)
+//} else{
+  //  alert(`Parabéns você acertou! O Número Secreto é  ${numeroSecreto} com ${Tentativas} Tentativa`)
+//}
+
+    
